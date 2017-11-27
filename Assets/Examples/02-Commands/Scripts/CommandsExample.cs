647d2f46896eca796b9c194e6292c20e32159c3a
Jerome Maurey-Delaunay <jerome@maureydelaunay.com>
2017-11-27 17:49:54 +0000

    Updated Commands Example: Added trigger command via notification use case. Updated UI.

--- Assets/Examples/02-Commands/Scripts/CommandsExample.cs
+++ Assets/Examples/02-Commands/Scripts/CommandsExample.cs
@@ -8,12 +8,24 @@ public class CommandsExample : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		if(NotificationManager.Instance != null) NotificationManager.Instance.RegisterNotificationInterest("UpdateCameraTarget", HandleUpdateCameraTarget);
-	}
 
+        // Listen to that notification and handle it here
+		if(NotificationManager.Instance != null)
+            NotificationManager.Instance.RegisterNotificationInterest("UpdateCameraTarget", HandleUpdateCameraTarget);
+
+        // Map same command to a notification:
+        if(NotificationToCommandRouter.Instance != null)
+            NotificationToCommandRouter.Instance.MapNotificationToCommand("UpdateCameraTargetMappedCommand", typeof(UpdateCameraTargetAndSpawnObjectCommand));
+
+    }
+
+    // Handle notification 
     private void HandleUpdateCameraTarget(Notification note)
     {
-        if( CommandManager.Instance != null) CommandManager.Instance.ExcuteCommand(typeof(UpdateCameraTargetAndSpawnObjectCommand));
+
+        if( CommandManager.Instance != null)
+            CommandManager.Instance.ExcuteCommand(typeof(UpdateCameraTargetAndSpawnObjectCommand));
+
     }
 
     // Update is called once per frame
